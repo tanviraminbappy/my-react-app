314dffbb5725b1b545eab7074ab4f1c3623cf18c
tanviraminbappy <tanvir.amin@infolytx.com>
2018-12-06 17:12:48 +0600

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PostList from './PostList'
+import PostList from './posts/PostList'
 
 class App extends Component {
   render() {
