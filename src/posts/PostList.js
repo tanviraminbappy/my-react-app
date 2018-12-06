ae0813f025abf5526576533a571d60b6f5e4e35c
tanviraminbappy <tanvir.amin@infolytx.com>
2018-12-06 17:45:28 +0600

    refactor postlist

--- src/posts/PostList.js
+++ src/posts/PostList.js
@@ -7,10 +7,10 @@ class PostList extends Component {
 
         return (<div>
 
-                <h1> Hello There</h1>
-                {PostData.map((posts, index) => {
+                <h1> <bold>Read All Posts</bold></h1>
+                {PostData.map((posts) => {
                     return <div>
-                        <h1>{posts.title}</h1>
+                        <h2>{posts.title}</h2>
                         <p>{posts.content}</p>
                         <span>{posts.slug}</span>
                     </div>
