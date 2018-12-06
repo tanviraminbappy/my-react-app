8257a02f4e8ec198ca1204ac0a2766740284ad37
tanviraminbappy <tanvir.amin@infolytx.com>
2018-12-06 17:05:41 +0600

    added post list

--- src/PostList.js
+++ src/PostList.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import PostData from './posts.json'
 
-class PostList extends  Component{
+class PostList extends Component {
 
-    render () {
+    render() {
 
-        return(<div>
+        return (<div>
 
-            <h1> Hello There</h1>
-        </div>
+                <h1> Hello There</h1>
+                {PostData.map((posts, index) => {
+                    return <div>
+                        <h1>{posts.title}</h1>
+                        <p>{posts.content}</p>
+                        <span>{posts.slug}</span>
+                    </div>
+                })}
+            </div>
         );
 
     }
