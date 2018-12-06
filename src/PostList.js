37736cf5230fd284af0bbcead2dbecf1543f603d
tanviraminbappy <tanvir.amin@infolytx.com>
2018-12-06 16:50:35 +0600

    added post list

---
+++ src/PostList.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class PostList extends  Component{
+
+    render () {
+
+        return(<div>
+
+            <h1> Hello There</h1>
+        </div>
+        );
+
+    }
+
+}
+
+export default PostList
