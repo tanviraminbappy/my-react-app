06d9cee3f4fa2a50c9974d4bf45e043668abf69f
tanviraminbappy <tanvir.swe@gmail.com>
2018-12-08 00:25:56 +0600

    learn js DOM

---
+++ src/basic-js/scripts.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("changeColor");
+
+button.addEventListener('click', () => {
+    let paragraph = document.getElementById("para");
+    paragraph.style.color = "yellow";
+    // document.body.style.color = "yellow";
+})
