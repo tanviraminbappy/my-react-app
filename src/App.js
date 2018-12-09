efe3856e9177f0b5abf3a3eb83ce23d540eb151a
tanviraminbappy <tanvir.amin@infolytx.com>
2018-12-09 18:02:49 +0600

    understand props and component

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return(
+        <img className="avatar" src={props.user.avatarUrl} alt={props.user.name}/>
+    );
+
+}
+
+function UserInfo(props) {
+    return(
+        <div className="user-info">
+            <Avatar user={props.author}/>
+            <div className="user-name">
+                {props.user.name}
+            </div>
+
+        </div>
+    );
+
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author}/>
+            <div className="Comment-text">{props.text}</div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+};
+
+class App extends Component {
+
+
+    render() {
+
+
+        return(
+            <div>
+            <Comment
+                date={comment.date}
+                text = {comment.text}
+                author = {comment.author}
+            />
+            </div>
+        );
+    }
+
+}
+
+export default App;
